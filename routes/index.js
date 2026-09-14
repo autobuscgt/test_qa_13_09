bd2a00fbcb069d340f0494d3e95b8f23a76ce6ec
autobus <lekiaaaskya@gmail.com>
2026-09-14 07:46:28 +0300

    fix router

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-const Router = require('express');
-const router = new Router();
+const express = require('express');
+const router = express.Router();
 
 const userRoutes = require('./userRoutes')
 const carRoutes = require('./carRoutes')
